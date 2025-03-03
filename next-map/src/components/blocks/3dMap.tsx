c97b128bcf9e8e8437c801a56f0a7d498cb69e1c
OtsukaNoboru <mopinfish@gmail.com>
2025-03-03 16:26:28 +0900

    add rekichizu as tile

--- next-map/src/components/blocks/3dMap.tsx
+++ next-map/src/components/blocks/3dMap.tsx
@@ -10,7 +10,8 @@ const styles: { [key: string]: maplibregl.StyleSpecification } = {
         type: 'raster',
         tiles: ['https://tile.openstreetmap.org/{z}/{x}/{y}.png'],
         tileSize: 256,
-        attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        attribution:
+          '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       },
     },
     layers: [
@@ -28,7 +29,7 @@ const styles: { [key: string]: maplibregl.StyleSpecification } = {
     sources: {
       konjaku: {
         type: 'raster',
-        tiles: ['https://cyberjapandata.gsi.go.jp/xyz/nankai/{z}/{x}/{y}.png'],
+        tiles: ['https://ktgis.net/kjmapw/kjtilemap/tokyo50/2man/{z}/{x}/{y}.png'],
         tileSize: 256,
         attribution: '今昔マップ',
       },
@@ -66,13 +67,14 @@ const styles: { [key: string]: maplibregl.StyleSpecification } = {
 }
 
 // Add PLATEAU source and layer to all styles
-Object.values(styles).forEach(style => {
+Object.values(styles).forEach((style) => {
   style.sources.plateau = {
     type: 'vector',
     tiles: ['https://indigo-lab.github.io/plateau-tokyo23ku-building-mvt-2020/{z}/{x}/{y}.pbf'],
     minzoom: 10,
     maxzoom: 16,
-    attribution: "データの出典:<a href='https://github.com/indigo-lab/plateau-tokyo23ku-building-mvt-2020'>plateau-tokyo23ku-building-mvt-2020 by indigo-lab</a> (<a href='https://www.mlit.go.jp/plateau/'>国土交通省 Project PLATEAU</a> のデータを加工して作成)",
+    attribution:
+      "データの出典:<a href='https://github.com/indigo-lab/plateau-tokyo23ku-building-mvt-2020'>plateau-tokyo23ku-building-mvt-2020 by indigo-lab</a> (<a href='https://www.mlit.go.jp/plateau/'>国土交通省 Project PLATEAU</a> のデータを加工して作成)",
   }
   style.layers.push({
     id: 'bldg',
@@ -95,67 +97,101 @@ const Map3D = () => {
   useEffect(() => {
     if (!mapContainer.current) return
 
-    const mapInstance = new maplibregl.Map({
-      container: mapContainer.current,
-      style: styles.osm,
-      center: [139.745461, 35.65856],
-      zoom: 14,
-      pitch: 45,
-      maxPitch: 85,
-    })
+    // rekichizuのスタイルを読み込む
+    fetch('https://mierune.github.io/rekichizu-style/styles/street/style.json')
+      .then((response) => response.json())
+      .then((rekichizuStyle: maplibregl.StyleSpecification) => {
+        // PLATEAUの定義を追加
+        rekichizuStyle.sources.plateau = {
+          type: 'vector',
+          tiles: [
+            'https://indigo-lab.github.io/plateau-tokyo23ku-building-mvt-2020/{z}/{x}/{y}.pbf',
+          ],
+          minzoom: 10,
+          maxzoom: 16,
+          attribution:
+            "データの出典:<a href='https://github.com/indigo-lab/plateau-tokyo23ku-building-mvt-2020'>plateau-tokyo23ku-building-mvt-2020 by indigo-lab</a> (<a href='https://www.mlit.go.jp/plateau/'>国土交通省 Project PLATEAU</a> のデータを加工して作成)",
+        }
+        rekichizuStyle.layers.push({
+          id: 'bldg',
+          type: 'fill-extrusion',
+          source: 'plateau',
+          'source-layer': 'bldg',
+          minzoom: 10,
+          maxzoom: 20,
+          paint: {
+            'fill-extrusion-color': '#797979',
+            'fill-extrusion-height': ['get', 'measuredHeight'],
+          },
+        })
 
-    mapInstance.addControl(new maplibregl.NavigationControl())
+        styles.rekichizu = rekichizuStyle
 
-    mapInstance.on('load', async () => {
-      const response = await fetch('./img/icon.png')
-      const blob = await response.blob()
-      const iconImage = await createImageBitmap(blob)
-      mapInstance.addImage('facility_icon', iconImage as ImageBitmap)
-      mapInstance.addSource('facility_point', {
-        type: 'geojson',
-        data: './tokyo_ks.geojson',
+        const mapInstance = new maplibregl.Map({
+          container: mapContainer.current as HTMLElement,
+          style: styles.osm,
+          center: [139.745461, 35.65856],
+          zoom: 14,
+          pitch: 45,
+          maxPitch: 85,
+        })
+
+        mapInstance.addControl(new maplibregl.NavigationControl())
+
+        mapInstance.on('load', async () => {
+          const response = await fetch('./img/icon.png')
+          const blob = await response.blob()
+          const iconImage = await createImageBitmap(blob)
+          mapInstance.addImage('facility_icon', iconImage as ImageBitmap)
+          mapInstance.addSource('facility_point', {
+            type: 'geojson',
+            data: './tokyo_ks.geojson',
+          })
+          mapInstance.addLayer({
+            id: 'facility_point',
+            type: 'symbol',
+            source: 'facility_point',
+            layout: {
+              'icon-image': 'facility_icon',
+              'icon-size': 0.1,
+            },
+          })
+        })
+
+        mapInstance.on('click', 'facility_point', (e) => {
+          if (e.features && e.features.length > 0) {
+            const feature = e.features[0]
+            const coordinates: [number, number] =
+              feature.geometry.type === 'Point'
+                ? ((feature.geometry as GeoJSON.Point).coordinates.slice(0, 2) as [number, number])
+                : [0, 0]
+
+            const name = feature.properties?.P12_001 || 'No name'
+
+            new maplibregl.Popup()
+              .setLngLat(coordinates)
+              .setHTML(`<h3>${name}</h3>`)
+              .addTo(mapInstance)
+          }
+        })
+
+        mapInstance.on('mouseenter', 'facility_point', () => {
+          mapInstance.getCanvas().style.cursor = 'pointer'
+        })
+        mapInstance.on('mouseleave', 'facility_point', () => {
+          mapInstance.getCanvas().style.cursor = ''
+        })
+
+        setMap(mapInstance)
+
+        return () => mapInstance.remove()
       })
-      mapInstance.addLayer({
-        id: 'facility_point',
-        type: 'symbol',
-        source: 'facility_point',
-        layout: {
-          'icon-image': 'facility_icon',
-          'icon-size': 0.1,
-        },
-      })
-    })
-
-    mapInstance.on('click', 'facility_point', (e) => {
-      if (e.features && e.features.length > 0) {
-        const feature = e.features[0]
-        const coordinates: [number, number] =
-          feature.geometry.type === 'Point'
-            ? ((feature.geometry as GeoJSON.Point).coordinates.slice(0, 2) as [number, number])
-            : [0, 0]
-
-        const name = feature.properties?.P12_001 || 'No name'
-
-        new maplibregl.Popup().setLngLat(coordinates).setHTML(`<h3>${name}</h3>`).addTo(mapInstance)
-      }
-    })
-
-    mapInstance.on('mouseenter', 'facility_point', () => {
-      mapInstance.getCanvas().style.cursor = 'pointer'
-    })
-    mapInstance.on('mouseleave', 'facility_point', () => {
-      mapInstance.getCanvas().style.cursor = ''
-    })
-
-    setMap(mapInstance)
-
-    return () => mapInstance.remove()
   }, [])
 
   const switchStyle = (styleKey: 'osm' | 'konjaku' | 'rekichizu') => {
     if (map) {
       map.setStyle(styles[styleKey])
-      
+
       // Re-add facility point layer after style change
       map.once('style.load', () => {
         map.addSource('facility_point', {
