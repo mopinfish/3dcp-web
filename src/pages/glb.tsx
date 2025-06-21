6e7b1698daaa1d037a1d6c27d60f107bc8a6bcac
OtsukaNoboru <mopinfish@gmail.com>
2025-06-21 15:58:58 +0900

    add glb page

---
+++ src/pages/glb.tsx
@@ -0,0 +1,10 @@
+import dynamic from 'next/dynamic'
+
+const GLBViewer = dynamic(() => import('../components/blocks/GlbViewer'), {
+  ssr: false,
+})
+
+export default function Page() {
+  console.log('GLB Viewer Page Loaded')
+  return <GLBViewer url="/data/kanmin_hub.glb" />
+}
