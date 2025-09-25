23f04c92ed3ed5a7c2c3b2c1b4975272bfe522f5
OtsukaNoboru <mopinfish@gmail.com>
2025-09-25 18:59:16 +0900

    add flyer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,11 @@ import React, { useEffect, useState } from 'react'
 import { LayoutWithFooter } from '@/components/layouts/Layout'
 import Article from '@/components/blocks/Article'
 import { cultural_property as culturalPropertyService } from '@/domains/services'
-import { CulturalProperties } from '@/domains/models/cultural_property'
+import { CulturalProperty, CulturalProperties } from '@/domains/models/cultural_property'
 import NavigationTab from '@/components/blocks/NavigationTab'
 import TranslationButton from '@/components/blocks/TranslationButton'
 import SearchConditionTab from '@/components/blocks/SearchConditionTab'
+import { get } from 'http'
 
 const Home = () => {
   const [properties, setProperties] = useState<CulturalProperties>([])
@@ -17,6 +18,17 @@ const Home = () => {
     }
     fetchData()
   }, [])
+
+  const getLinkHref = (property: CulturalProperty) => {
+    if (property.movies.length > 0) {
+      if (property.movies[0].url.includes('lumalabs')) {
+        return `/luma/${property.movies[0].id}`
+      } else {
+        return property.movies[0].url
+      }
+    }
+    return '#'
+  }
 
   return (
     <LayoutWithFooter>
@@ -30,7 +42,7 @@ const Home = () => {
           imageUrls={property.images.map((image) => image.image)}
           title={property.name}
           description={property.note ?? ''}
-          linkHref={`/luma/${property.movies[0]?.id}`}
+          linkHref={getLinkHref(property)}
           linkText="大きな画面で3Dモデルを見る"
           movieId={property.movies[0]?.id}
         />
